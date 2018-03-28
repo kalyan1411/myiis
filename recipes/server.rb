411494453884e81796c657aa31a94f4737c40408
A Learner <kalyan1411>
2018-03-28 21:40:46 +0000

    added content to server.rb

--- recipes/server.rb
+++ recipes/server.rb
@@ -2,7 +2,11 @@ powershell_script 'Install IIS' do
   code 'Add-WindowsFeature Web-Server'
 end
 file 'C:\inetpub\wwwroot\Default.htm' do
-  content '<h1>Hello, world!</h1>'
+  content "<h1>Hello, world!</h1>
+  <h2>PLATFORM: #{node['platform']}</h2>
+  <h2>HOSTNAME: #{node['hostname']}</h2>
+  <h2>MEMORY: #{node['memory']['total']}</h2>
+  <h2>CPU CORES: #{node['cpu']['cores']}</h2>"
 end
 service 'w3svc' do
   action [ :start, :enable ]
